98d03850fdd90bf566f73237ad6a24400dc20ffc
PRATHAMESH V SHENOY <prathameshvshenoy@gmail.com>
2026-02-08 00:04:26 +0530

    Your commit message

--- backend/index.js
+++ backend/index.js
@@ -29,24 +29,6 @@ app.post("/profiles", async (req, res) => {
 });
 app.use(express.json());
 
-let profiles = [
-  { id: 1, name: "KSRTC", lat: 12.9716, lng: 77.5946 },
-  { id: 2, name: "Suresh Motors", lat: 12.9716, lng: 77.5946 }
-];
-
-app.delete("/profiles/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  const index = profiles.findIndex(p => p.id === id);
-  if (index === -1) {
-    return res.status(404).json({ message: "Profile not found" });
-  }
-
-  profiles.splice(index, 1);
-  res.json({ message: "Deleted successfully" });
-});
-
-
 app.listen(5000, () => {
   console.log("✅ Backend running on http://localhost:5000");
 });
